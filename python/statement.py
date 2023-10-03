a8a3e7eb1ebcad89ca357f9e83cff0701c87c2d1
Klaus Cinibulk <klaus.cinibulk@rbinternational.com>
2023-10-03 13:22:34 +0200

    refactor: add html renderer

--- python/statement.py
+++ python/statement.py
@@ -5,18 +5,22 @@ import functools
 from typing import Iterable, TypedDict
 
 
-AllPlays = dict[str, dict[str, str]]
-
 # Just to have a little type-safety
 class Performance(TypedDict):
     playID: str
     audience: int
 
-
 # Just to have a little type-safety
 class Invoice(TypedDict):
     customer: str
     performances: list[Performance]
+
+# Just to have a little type-safety
+class Play(TypedDict):
+    name: str
+    type: str
+
+AllPlays = dict[str, Play]
 
 
 @dataclass(frozen=True)
@@ -34,84 +38,105 @@ class Statement:
     statement_lines: Iterable[StatementLine]
     credits: int
 
-
-def render_as_text(statement: Statement) -> str:
-    result = f"Statement for {statement.customer}\n"
-    total = 0
-    for line in statement.statement_lines:
-        result += f" {line.play_name}: {format_as_dollars(line.amount)} ({line.seats} seats)\n"
-        total += line.amount
-    result += f"Amount owed is {format_as_dollars(total)}\n"
-    result += f"You earned {statement.credits} credits\n"
-    return result
-
-
-class BillingStrategy(ABC):
-    def __init__(self, name: str):
-        self._name = name
-
     @property
-    def play(self):
-        return self._name
-
-    @abstractmethod
-    def amount(self, perf: Performance) -> int:
-        ...
-
-    @abstractmethod
-    def credits(self, perf: Performance) -> int:
-        ...
-
-
-class TragedyBilling(BillingStrategy):
-    def amount(self, perf: Performance) -> int:
-        amount = 40000
-        if perf["audience"] > 30:
-            amount += 1000 * (perf["audience"] - 30)
-        return amount
-
-    def credits(self, perf: Performance):
-        return max(perf["audience"] - 30, 0)
-
-
-class ComedyBilling(BillingStrategy):
-    def amount(self, perf: Performance) -> int:
-        amount = 30000 + 300 * perf["audience"]
-        if perf["audience"] > 20:
-            amount += 10000 + 500 * (perf["audience"] - 20)
-        return amount
-
-    def credits(self, perf: Performance):
-        return max(perf["audience"] - 30, 0) + perf["audience"] // 5
+    def total(self):
+        return sum(s.amount for s in self.statement_lines)
 
 
 def format_as_dollars(cents: int) -> str:
     return f"${cents/100:0,.2f}"
 
 
-def create_billing(plays: AllPlays, perf: Performance) -> BillingStrategy:
-    play_type, play_name = plays[perf["playID"]]["type"], plays[perf["playID"]]["name"]
+def render_as_text(statement: Statement) -> str:
+    result = f"Statement for {statement.customer}\n"
+    for line in statement.statement_lines:
+        result += f" {line.play_name}: {format_as_dollars(line.amount)} ({line.seats} seats)\n"
+    result += f"Amount owed is {format_as_dollars(statement.total)}\n"
+    result += f"You earned {statement.credits} credits\n"
+    return result
+
+
+def render_as_html(statement: Statement) -> str:
+    result = f"<h1>Statement for {statement.customer}</h1>\n"
+    result += "<table>\n"
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n"
+    for line in statement.statement_lines:
+        result += f" <tr><td>{line.play_name}</td><td>{line.seats}</td>"
+        result += f"<td>{format_as_dollars(statement.total)}</td></tr>\n"
+    result += "</table>\n"
+    result += f"<p>Amount owed is <em>{format_as_dollars(statement.total)}</em></p>\n"
+    result += f"<p>You earned <em>{statement.credits}</em> credits</p>"
+    return result
+
+
+class BillingStrategy(ABC):
+    def __init__(self, play: Play, perf: Performance):
+        self._play = play
+        self._perf = perf
+
+    @property
+    def name(self) -> str:
+        return self._play["name"]
+
+    @property
+    def audience(self) -> int:
+        return self._perf["audience"]
+
+    def credits(self) -> int:
+        return max(self.audience - 30, 0)
+
+    def statement_line(self) -> StatementLine:
+        return StatementLine(self.name, self.amount(), self.audience)
+
+    @abstractmethod
+    def amount(self) -> int:
+        ...
+
+
+class TragedyBilling(BillingStrategy):
+    def amount(self) -> int:
+        amount = 40000
+        if self.audience > 30:
+            amount += 1000 * (self.audience - 30)
+        return amount
+
+
+class ComedyBilling(BillingStrategy):
+    def amount(self) -> int:
+        amount = 30000 + 300 * self.audience
+        if self.audience > 20:
+            amount += 10000 + 500 * (self.audience - 20)
+        return amount
+
+    def credits(self) -> int:
+        return super().credits() + self.audience // 5
+
+# Add new BillingStrategies here
+
+def billing_strategy(plays: AllPlays, perf: Performance) -> BillingStrategy:
+    play_type = plays[perf["playID"]]["type"]
     match play_type:
         case "tragedy":
-            return TragedyBilling(play_name)
+            return TragedyBilling(plays[perf["playID"]], perf)
         case "comedy":
-            return ComedyBilling(play_name)
+            return ComedyBilling(plays[perf["playID"]], perf)
+        # Insert new play_types here
         case _:
             raise ValueError(f"unknown type: {play_type}")
 
 
 def calculate(invoice: Invoice, plays: AllPlays) -> Statement:
-    perfs = invoice["performances"]
-    all_billings = [create_billing(plays, perf) for perf in perfs]
-    return Statement(
-        invoice["customer"],
-        (
-            StatementLine(billing.play, billing.amount(perf), perf["audience"])
-            for billing, perf in zip(all_billings, perfs)
-        ),
-        sum(billing.credits(perf) for billing, perf in zip(all_billings, perfs))
+    lines, credits = functools.reduce(lambda total, strategy:
+        (total[0] + [strategy.statement_line()], total[1] + strategy.credits()),
+        (billing_strategy(plays, perf) for perf in invoice["performances"]),
+        ([], 0)
     )
+    return Statement(invoice["customer"], lines, credits)
 
 
 def statement(invoice: Invoice, plays: AllPlays) -> str:
     return render_as_text(calculate(invoice, plays))
+
+
+def html_statement(invoice: Invoice, plays: AllPlays) -> str:
+    return render_as_html(calculate(invoice, plays))
--- python/tests/test_statement.py
+++ python/tests/test_statement.py
@@ -4,7 +4,7 @@ import pytest
 from approvaltests import verify
 from approval_utilities.utils import get_adjacent_file
 
-from statement import statement
+from statement import html_statement, statement
 
 
 def test_example_statement():
@@ -15,6 +15,14 @@ def test_example_statement():
     verify(statement(invoice, plays))
 
 
+def test_example_html_statement():
+    with open(get_adjacent_file("invoice.json")) as f:
+        invoice = json.loads(f.read())
+    with open(get_adjacent_file("plays.json")) as f:
+        plays = json.loads(f.read())
+    verify(html_statement(invoice, plays))
+
+
 def test_statement_with_new_play_types():
     with open(get_adjacent_file("invoice_new_plays.json")) as f:
         invoice = json.loads(f.read())
@@ -23,3 +31,5 @@ def test_statement_with_new_play_types():
     with pytest.raises(ValueError) as exception_info:
         statement(invoice, plays)
     assert "unknown type" in str(exception_info.value)
+
+
